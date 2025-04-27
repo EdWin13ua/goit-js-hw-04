34f396d02ebab1350d2407bdd256174a559d912e
Ivan Sinyutin <ivan.sinyutin@gmail.com>
2025-04-27 23:19:35 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,22 @@
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredNumbers.push(numbers[i]);
+    }
+  }
+
+  return filteredNumbers;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+console.log(filterArray([5, 10, 15, 20, 25], 18));
+console.log(filterArray([7, 14, 21, 28, 35], 25));
